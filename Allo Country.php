69f02eeb2551eada48703b4752cb4de8f4edd243
Nicolas Surville <36602543+Deckmir@users.noreply.github.com>
2018-08-27 22:14:14 +0200

    Add files via upload

---
+++ Allo Country.php
@@ -0,0 +1,83 @@
+<?php 
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
+
+if(isset($_POST['forminscription']))
+{
+	echo 'ok';
+}
+?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+</head>
+<body>
+ 	<div>	
+ 		<form method="POST" action="">
+ 			<table > 
+
+ 				<tr>
+ 					<td align="right">
+ 				<label for ="username">Username :</label>
+ 					</td>
+ 					<td>
+ 				<input type="text" placeholder="Your username" id="username" name="username">
+ 					</td>
+
+ 				</tr>
+
+
+ 				<tr>
+ 					<td align="right">
+ 				<label for ="password">Password :</label>
+ 					</td>
+ 					<td>
+ 				<input type="password" placeholder="Your password" id="password" name="password">
+ 					</td>
+ 				</tr>
+
+				<tr>
+ 					<td align="right">
+ 				<label for ="password2">Confimation password :</label>
+ 					</td>
+ 					<td>
+ 				<input type="password" placeholder="Confirm your password" id="password2" name="password">
+ 					</td>
+ 				</tr>
+
+
+
+ 				<tr>
+ 					<td align="right">
+ 				<label for ="mail">Mail :</label>
+ 					</td>
+ 					<td>
+ 				<input type="email" placeholder="Your mail" id="mail" name="mail">
+ 					</td>
+ 				</tr>
+
+ 				<tr>
+ 					<td align="right">
+ 				<label for ="mail2">Confirmation mail :</label>
+ 					</td>
+ 					<td>
+ 				<input type="email" placeholder="Confirm your mail" id="mail2" name="mail">
+ 					</td>
+
+ 				</tr>
+ 				<tr>
+ 					<td ></td> 
+ 					<td align="center"> 	<input type="submit" name="forminscription" value="Register" > </td>
+ 				</tr>
+
+ 			</table>
+ 		
+ 			
+ 			  
+
+ 		</form>
+
+ 	</div>
+</body>
+</html>
