ab21c0d444bfe91f30e8a885e5f46a61a64aa5e1
Nicolas Surville <36602543+Deckmir@users.noreply.github.com>
2018-08-28 19:21:03 +0200

    Uptade 28/28

--- Allo Country.php
+++ Allo Country.php
@@ -1,12 +1,4 @@
-<?php 
 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
-
-if(isset($_POST['forminscription']))
-{
-	echo 'ok';
-}
-?>
 <html>
 <head>
   <meta charset="utf-8">
