59435b4ea5300b6d5b4d7c53bdbfe742a95c2c11
Nicolas Surville <36602543+Deckmir@users.noreply.github.com>
2018-08-28 22:43:16 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -87,69 +87,28 @@ new Vue({
 });
 
 
-
-
-
-
-Vue.component('star-rating', {
-
-  props: {
-    'name': String,
-    'value': null,
-    'id': String,
-    'disabled': Boolean,
-    'required': Boolean
-  },
-
-  template: '<div class="star-rating">\
-        <label class="star-rating__star" v-for="rating in ratings" \
-        :class="{\'is-selected\': ((value >= rating) && value != null), \'is-disabled\': disabled}" \
-        v-on:click="set(rating)" v-on:mouseover="star_over(rating)" v-on:mouseout="star_out">\
-        <input class="star-rating star-rating__checkbox" type="radio" :value="rating" :name="name" \
-        v-model="value" :disabled="disabled">★</label></div>',
-
-  data: function() {
-    return {
-      temp_value: null,
-      ratings: [1, 2, 3, 4, 5]
-    };
-  },
-
-  methods: {
-    
-    star_over: function(index) {
-      var self = this;
-
-      if (!this.disabled) {
-        this.temp_value = this.value;
-        return this.value = index;
-      }
-
-    },
-
-    
-    star_out: function() {
-      var self = this;
-
-      if (!this.disabled) {
-        return this.value = this.temp_value;
-      }
-    },
-
-    /*
-     * Set the rating.
-     */
-    set: function(value) {
-      var self = this;
-
-      if (!this.disabled) {
-        this.temp_value = value;
-        return this.value = value;
-      }
+var rating="";
+function starmark(item)
+{
+  var count=item.id[0];
+  rating = count;
+  var subid=item.id.substring(1);
+  for(var i=0;i<10;i++)
+  {
+    if(i<count)
+    {
+      document.getElementById((i+1)+subid).style.color="orange";
+    }
+    else{
+      document.getElementById((i+1)+subid).style.color="black";
     }
   }
-});
 
-new Vue({
-  el: '#star'
-});
+}
+
+
+
+function showRating()
+{
+  alert(rating);
+}
